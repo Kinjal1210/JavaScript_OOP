4702be898e2359c55bc349311013c4f410ffe489
kinjal patel <kinjalpatel@kinjals-MacBook-Air.local>
2023-02-09 16:41:11 -0500

    updated

--- app.js
+++ app.js
@@ -4,115 +4,97 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 class Person {
-    constructore (name, pets, residence, hobbies){
+    constructor(name, pets, residence, hobbies){
         this.name = name;
         this.pets = pets;
         this.residence = residence;
         this.hobbies = hobbies;
+    }  
+      
+   addHobby(hobby){
+    this.hobbies.push(hobby);
+    }
 
+   removeHobby(hobby){
+    this.hobbies = this.hobbies.filter(elements => elements !== hobby);
+    }
+
+   greetings(){ 
+    console.log(`Hello fellow person!`);
+    }
+}
+ const testPerson = new Person("Riddhi", 1, "Indiana"); 
+
+ console.log(testPerson);
+
+
+//Exercise 2 Section
+ console.log("EXERCISE 2:\n==========\n");
+
+class Coder extends Person {
+    constructor(name, pets,residence, hobbies){
+        super(name, pets, residence, hobbies);
+        this.occupation = "Full Stack Web Developer";
+    }
+    greetings(){
+        console.log(`Hello, world`);
     }
     
-    addHobby(hobby){
-        this.hobbies.push(hobby);
-    }
-    
-    removeHobby(rmhobby) { 
-     let myIdx = this.hobbies.indexOf(rmhobby);
-     this.hobbies.splice(myIdx, 1);
-
-    }
-       
-    greeting() {
-        console.log("Hello fellow person");
-    } 
-        
-}  
-
-
-// Exercise 2 //
-
-class  Coder extends Person {
-    constructor(name, pets, residence, hobbies, occupation) {
-         super(name, pets, residence, hobbies);
-         this.occupation = "Full stack Web Developer";
-    }
-
-    greeting() {
-         console.log("Hello I am a coder!");
-   }
 }
 
-const myCoder = new Coder("Kinjal", 4, "indiana",["feeling warm", "drink coffee", "playing"]);
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+const tPerson = new Person("Kunjal", 1, "Indiana"[ "Playing"]); 
+
+console.log(tPerson.hobbies);
+
+let myCoder = new Coder("Kunjal", 0, "Indiana", ["reading", "Playing"]);
+
 console.log(myCoder);
 
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
 
-// // Exercise 3 //
-
-const testPerson = new Person ("Kinjal", 12, "Indiana", ["cricket", "football", "games",]); 
-
-
-console.log(testPerson.hobbies.slice());
-testPerson.removeHobby("football");
-console.log(testPerson.hobbies);
-
-
-
-
-
-// Exercise 4 //
-
-class Calculator {
+class Calculater {
     constructor(){
-        this.result = 0;
+     this.return = 0;
     }
 
-    
-    add(a, b) {
-        if (b != undefined){
+    add(a,b) {
         this.result = a + b;
-        }else{
-            this.result += a;
-        }   
-        return this.result
-    }
+        return this.result;
 
-    subtract(a, b){
-        if (b != undefined){
-            this.result = a - b;
-        }else{
-            this.result -= a;
-        } 
-        return this.result
     }
+    subtract(a, b) {
+        this.result = a - b;
+        return this.result;
 
-    multiply(a, b){
-        if (b != undefined){
-            this.result = a * b;
-            }else{
-                this.result *= a;
-            }  
-            return this.result
+
     }
+    multiply(a, b) {
+        this.result = a * b;
+        return this.result;
 
-    divide(a, b){
-        if (b != undefined){
-            this.result = a / b;
-            }else{
-                this.result /= a;
-            }    
-            return this.result
+
     }
-
-    displayResult(){
-        console.log(`cyrrent Result is ${this.result}`);
+    divide(a, b) {
+        this.result = a / b;
+        return this.result;
+    }
+    display() {
+        console.log(this.result);
     }
 }
+ let calc = new Calculater();
 
-const myCalculator = new Calculator();
-myCalculator.displayResult();
+ calc.add(2, 1);
+ calc.display()
+ calc.subtract(7, 3);
+ calc.display()
+ calc.multiply(8, 3);
+ calc.display()
+ calc.divide(8, 40);
+ calc.display()
 
-console.log(myCalculator.result);
-console.log(myCalculator.add(10, 2));
-console.log(myCalculator.add(10, 0));
-console.log(myCalculator.add(5));
 
